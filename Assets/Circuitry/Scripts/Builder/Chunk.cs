5d11fdd63916df4ffbf1d62da00c4b32188531e8
Thiago <ThiagoDAraujoS@Gmail.com>
2020-04-06 12:36:18 -0700

    baseline architecture started adding new class files and class diagram

---
+++ Assets/Circuitry/Scripts/Builder/Chunk.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Circuitry.Builder
+{
+    public class Chunk : MonoBehaviour
+    {
+        public Coordinate Size { get; set; }
+        public Coordinate Position { get; set; }
+        public List<Chunk> Conections { get; set; }
+        public int ColorId { get; set; }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
